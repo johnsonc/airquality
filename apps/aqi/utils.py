7fd879f3cfa41bec97064f1117be4b61b9958901
Johnson Chetty <johnsonchetty@gmail.com>
2015-07-22 16:48:03 +0530

    Added test data code utils.py

---
+++ apps/aqi/utils.py
@@ -0,0 +1,59 @@
+from datetime import date, datetime, timedelta
+
+import random
+from models import AQFeed, AQDevice
+
+def datespan(startDate, endDate, delta=timedelta(days=1)):
+    currentDate = startDate
+    while currentDate < endDate:
+        yield currentDate
+        currentDate += delta
+
+    
+
+def generate_random_data(start_date, end_date, imei, name, lat, lon):
+    try:
+        start_time = datetime(*[int(i) for i in start_date.split("-")])
+        end_time = datetime(*[int(i) for i in end_date.split("-")])
+        #assert (imei=="" or imei== None), "IMEI cannot be blank"
+        #assert (name=="" or name== None), "Name cannot be blank"
+        #assert (lat=="" or lat== None), "Lat cannot be blank"
+        #assert (lon=="" or lon== None), "Long cannot be blank"
+        for ts in datespan(start_time, end_time, delta=timedelta(minutes=5)):
+            aqf = AQFeed()
+            aqf.imei=imei
+            aqf.name=name            
+            aqf.humidity = random.triangular(1,100) + random.random()
+            aqf.temperature = random.triangular(15,45) + random.random()
+            aqf.pm10 = random.triangular(1,100) + random.random()
+            aqf.pm25 = random.triangular(1,100) + random.random()
+            aqf.count_large = random.triangular(1,100) + random.random()
+            aqf.count_small = random.triangular(1,100) + random.random()
+            aqf.lat = lat
+            aqf.lon = lon
+            aqf.created_on=ts
+            aqf.save()                    
+    except: 
+        print "Usage::"
+        print  "generate_random_data(start_date, end_date, imei, name, lat, lon)" 
+        print "Format of dates must be as :: YYYY-MM-DD-HH-MM "
+        return 
+        
+
+
+def do(num=10, start_time='2015-06-22-12-23', end_time='2015-07-22-16-00'):
+    for i in range(num):
+        lat = random.triangular(18.3,20) + random.random()
+        lon = random.triangular(72.5,73) + random.random()
+        name_prefix='aqid'
+        imei= str(random.random())[2:15]
+        aqd = AQDevice()
+        aqd.imei=imei
+        aqd.title="IndiaSpend Air Quality Device " + str(i + 1)
+        aqd.url = name_prefix + str(i)
+        aqd.desc = "Batch: " + str(1)  + ". Given to XYZ Inc. "
+        aqd.lat = lat
+        aqd.lon = lon
+        aqd.geom = {u'type': u'Point', u'coordinates': [lon,lat]}
+        aqd.save()
+        #generate_random_data(start_time,end_time,imei, url, lat, lon)
