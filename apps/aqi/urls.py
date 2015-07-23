44ec778bda1ce5f577c96cf6a428840e45033a1d
Johnson Chetty <johnson@gmail.com>
2015-07-23 15:32:51 +0530

    Merged with serverdev

--- apps/aqi/urls.py
+++ apps/aqi/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 import views as aqviews
 
 urlpatterns = patterns('',
-    url(r'^devices/$', aqviews.aqdevice_list),
-    url(r'^devices/(?P<pk>[a-z\d]+)/$', aqviews.aqdevice_detail),
+    url(r'devices/$', aqviews.aqdevice_list),
+    url(r'devices/(?P<pk>[a-z\d]+)/$', aqviews.aqdevice_detail),
+    url(r'aqfeed/$', aqviews.aqfeed_list),
+    url(r'aqfeed/(?P<pk>[a-z\d]+)/$', aqviews.aqfeed_detail),
                        )
